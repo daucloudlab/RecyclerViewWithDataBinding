fe9700b9f89fdadb720e3fe78974e36bacf849d2
daulet <daucloudlab@gmail.com>
2020-07-14 20:39:55 +0600

    RecyclerView with DataBinding

---
+++ app/src/main/java/kz/alamyn/databindingrecyclerview/EmployeeDataService.java
@@ -0,0 +1,9 @@
+package kz.alamyn.databindingrecyclerview;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface EmployeeDataService {
+    @GET("users/?per_page=12&page=1")
+    Call<EmployeeDBResponse> getEmployees();
+}
---
+++ app/src/main/java/kz/alamyn/databindingrecyclerview/MainViewModel.java
@@ -0,0 +1,22 @@
+package kz.alamyn.databindingrecyclerview;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class MainViewModel extends AndroidViewModel {
+    private EmployeeRepository employeeRepository;
+
+    public MainViewModel(@NonNull Application application) {
+        super(application);
+        employeeRepository = new EmployeeRepository();
+    }
+
+    public LiveData<List<Employee>> getAllEmployee() {
+        return employeeRepository.getMutableLiveData();
+    }
+}
